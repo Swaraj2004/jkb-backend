6b42091c2d9e99f56d9e637634a8f793d8d76f1d
Aum Patel <aumpatel131@gmail.com>
2025-03-26 23:28:26 +0530

    complete payment Routes

---
+++ src/models/paymet_req_body.ts
@@ -0,0 +1,12 @@
+// this is a custom body which we will receive when creating a new payment
+export interface PaymentBody {
+    mode: string;           // futher improvement can be done by making this of fix enum type
+    amount: number;
+    pending: number;
+    is_gst: boolean;
+    status: string;
+    student_id: string;
+    staff_id: string;
+    remark?: string;
+    receipt_number?: string;
+}
