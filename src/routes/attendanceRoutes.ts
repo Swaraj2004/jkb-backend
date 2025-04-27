1a0a2e69275cf19b3b5e113922ebcd5ef6c204a0
Aum Patel <aumpatel131@gmail.com>
2025-04-27 20:19:13 +0530

    made some fixes

--- src/routes/attendanceRoutes.ts
+++ src/routes/attendanceRoutes.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { AuthenticatedRequest, authMiddleware, authorizeRoles } from '../middlewares/authMiddleware';
 import { getLectureAttendance, getStudentAttendance, markAttendance } from '../controllers/attendanceController';
-import { PROFESSOR_ROLE, STUDENT_ROLE } from '../utils/consts';
+import { PROFESSOR_ROLE, STATUS_CODES, STUDENT_ROLE } from '../utils/consts';
 
 const router = express.Router();
 
@@ -29,9 +29,9 @@ const router = express.Router();
  *       200:
  *         description: A list of student attendance records
  */
-router.get('/lectures/:lecture_id/attendance', authMiddleware, authorizeRoles([PROFESSOR_ROLE]), async (req: Request, res: Response) => {
-    const lectureId = req.params.lecture_id;
-    return getLectureAttendance(req, res, lectureId);
+router.get('/lectures/:lecture_id/attendance', authMiddleware, authorizeRoles([PROFESSOR_ROLE]), async (req: Request, res: Response): Promise<void> => {
+  const lectureId = req.params.lecture_id;
+  return getLectureAttendance(req, res, lectureId);
 });
 
 /**
@@ -51,18 +51,18 @@ router.get('/lectures/:lecture_id/attendance', authMiddleware, authorizeRoles([P
  *       200:
  *         description: Attendance toggle updated successfully
  */
-router.put('/lectures/:lecture_id/toggle-attendance', async (req: Request, res: Response) => {
-    // const lectureId = req.params.lecture_id; // Get lecture ID from the path parameters
-    // const updateData = { record_id: lectureId, attendance_toggle: false };
+router.put('/lectures/:lecture_id/toggle-attendance', async (req: Request, res: Response): Promise<void> => {
+  // const lectureId = req.params.lecture_id; // Get lecture ID from the path parameters
+  // const updateData = { record_id: lectureId, attendance_toggle: false };
 
-    // const updatedLecture = await webController.update_record(
-    //     dbInstance,
-    //     req.user,
-    //     new LectureUpdateToggle(updateData),
-    //     LECTURE_COLLECTION_NAME
-    // );
+  // const updatedLecture = await webController.update_record(
+  //     dbInstance,
+  //     req.user,
+  //     new LectureUpdateToggle(updateData),
+  //     LECTURE_COLLECTION_NAME
+  // );
 
-    res.send(500).json({ "message": "Never Implemented does it even exsits?" });
+  res.send(STATUS_CODES.BAD_REQUEST).json({ "message": "Never Implemented does it even exsits?" });
 });
 
 /**
@@ -81,9 +81,9 @@ router.put('/lectures/:lecture_id/toggle-attendance', async (req: Request, res: 
  *       201:
  *         description: Attendance marked successfully
  */
-router.post('/student/mark-attendance', authMiddleware, authorizeRoles([STUDENT_ROLE, PROFESSOR_ROLE]), async (req, res) => {
-    const { lecture_id, student_id } = req.body;
-    return markAttendance(req, res, lecture_id, student_id);
+router.post('/student/mark-attendance', authMiddleware, authorizeRoles([STUDENT_ROLE, PROFESSOR_ROLE]), async (req: Request, res: Response): Promise<void> => {
+  const { lecture_id, student_id } = req.body;
+  return markAttendance(req, res, lecture_id, student_id);
 });
 
 /**
@@ -103,23 +103,23 @@ router.post('/student/mark-attendance', authMiddleware, authorizeRoles([STUDENT_
  *       200:
  *         description: A list of attendance records for the student
  */
-router.get('/student/attendance', authMiddleware, authorizeRoles([STUDENT_ROLE, PROFESSOR_ROLE]), async (req: AuthenticatedRequest, res: Response) => {
-    // if (!req.user) {
-    //     res.status(401).json(errorJson("Please log in first", null));
-    //     return;
-    // }
-    
-    // if (
-    //     !AUTH_ROLES.includes(req.user.role_name) &&
-    //     req.user.role_name !== PROFESSOR_ROLE &&
-    //     req.user.id != req.params.user_id
-    // ) {
-    //     res.status(403).json(errorJson("Unauthorized", null));
-    //     return;
-    // }
-    
-    const studentId = req.query.student_id;
-    return getStudentAttendance(req, res, studentId as string);
+router.get('/student/attendance', authMiddleware, authorizeRoles([STUDENT_ROLE, PROFESSOR_ROLE]), async (req: AuthenticatedRequest, res: Response): Promise<void> => {
+  // if (!req.user) {
+  //     res.status(401).json(errorJson("Please log in first", null));
+  //     return;
+  // }
+
+  // if (
+  //     !AUTH_ROLES.includes(req.user.role_name) &&
+  //     req.user.role_name !== PROFESSOR_ROLE &&
+  //     req.user.id != req.params.user_id
+  // ) {
+  //     res.status(403).json(errorJson("Unauthorized", null));
+  //     return;
+  // }
+
+  const studentId = req.query.student_id;
+  return getStudentAttendance(req, res, studentId as string);
 });
 
 export default router;
